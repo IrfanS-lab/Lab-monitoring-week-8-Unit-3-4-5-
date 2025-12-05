f9fa553432205e64c5e4a796a0797734fc58f163
anismaisarah1602 <117556456+anismaisarah1602@users.noreply.github.com>
2025-12-05 14:55:49 +0800

    update MainActivity UI

---
+++ app/src/main/java/com/example/basic_android_kotlin_compose_training_courses/model/Topic.kt
@@ -0,0 +1,10 @@
+package com.example.basic_android_kotlin_compose_training_courses.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
